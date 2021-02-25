d2860d573a48a9412b0b41bfcb50ba4063ee3cf4
jessicamelise <jessica.melise@hotmail.com>
2021-02-24 23:03:47 -0300

    html e css globais, index com imports dos components estáticos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
-import Head from 'next/head';
+import { Profile } from '../components/Perfil';
 import { ExperienceBar } from '../components/ExperienceBar';
+import styles from '../styles/components/Home.module.css';
+import { CompletedChallenges } from '../components/completedChallenges';
+import { Countdown } from '../components/Countdown';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={ styles.container }>
       <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+        <title>Início | move.it</title>
       </Head>
-
       <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
